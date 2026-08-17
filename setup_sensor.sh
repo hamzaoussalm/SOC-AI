1eddd9e82dc748d78ea94d949a107ad55108d8a6
Hamza <hamzaoussalm17@gmail.com>
2026-08-17 05:23:37 -0600

    Added official Zeek repository keys

--- setup_sensor.sh
+++ setup_sensor.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 echo "Updating Ubuntu package list..."
 sudo apt-get update -y
+sudo apt-get install -y curl gnupg2
 
-echo "Installing Core Sensor Tools (Tcpdump, Suricata, Zeek, Tshark, Python3)..."
+echo "Adding the official Zeek Repository..."
+curl -fsSL https://download.opensuse.org/repositories/security:zeek/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/security_zeek.gpg > /dev/null
+echo 'deb http://download.opensuse.org/repositories/security:/zeek/xUbuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/security:zeek.list
+sudo apt-get update -y
+
+echo "Installing Core Sensor Tools..."
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get install -y tcpdump suricata zeek tshark python3 python3-pip
 

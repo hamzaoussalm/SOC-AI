fa32aaa205df1c3a159814bbe1ef97910c8d557d
Hamza <hamzaoussalm17@gmail.com>
2026-08-16 18:44:06 -0600

    Added OpenSSH to Target setup

--- setup_target.sh
+++ setup_target.sh
@@ -2,11 +2,13 @@
 echo "Updating Ubuntu package list..."
 sudo apt-get update -y
 
-echo "Installing Apache Web Server..."
-sudo apt-get install -y apache2
+echo "Installing Apache Web Server and OpenSSH Server..."
+sudo apt-get install -y apache2 openssh-server
 
-echo "Starting and enabling Apache service..."
+echo "Starting and enabling services..."
 sudo systemctl start apache2
 sudo systemctl enable apache2
+sudo systemctl start ssh
+sudo systemctl enable ssh
 
-echo "Target web server setup is complete!"
+echo "Target web server and SSH setup is complete!"
